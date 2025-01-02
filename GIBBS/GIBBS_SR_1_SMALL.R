580fc34a18168f18eaf4cd0ea416bb9ef001a340
Jakub Pypkowski <99726949+jpypkowski@users.noreply.github.com>
2025-01-02 16:53:15 +0000

    Simulation: Gibbs process

---
+++ GIBBS/GIBBS_SR_1_SMALL.R
@@ -0,0 +1,422 @@
+library(spatstat)
+library(doParallel)
+
+# SIMULATION WINDOW GEOMETRIC - AUXILIARY FUNCTIONS
+simWind <- function(xdim, ydim){
+  xDiff <- xdim[2]-xdim[1]
+  yDiff <- ydim[2]-ydim[1]
+  winSize <- xDiff*yDiff
+  if(xDiff < yDiff){
+    xlim <- c(mean(xdim)-yDiff/2, mean(xdim)+yDiff/2)
+    ylim <- ydim
+  } else if(yDiff < xDiff){
+    ylim <- c(mean(ydim)-xDiff/2, mean(ydim)+xDiff/2)
+    xlim <- xdim
+  } else {
+    xlim <- xdim
+    ylim <- ydim
+  }
+  return(list(winSize, winBor=cbind(c(xdim[1],xdim[2],xdim[2],xdim[1]),
+                                    c(ydim[1],ydim[1],ydim[2],ydim[2])),
+              lim=cbind(xlim, ylim), xDiff=xDiff, yDiff=yDiff))
+}
+
+
+# HOMOGENOUS POISSON POINT PROCES SIMULATION
+rHomPois <- function(intensity=NA, xdim, ydim, numPoints=NA){
+  window <-simWind(xdim, ydim)
+  winSize <- window[[1]]
+  if(is.na(numPoints)) numPoints <- rpois(1, intensity*winSize)
+  x <- runif(numPoints, min=xdim[1], max=xdim[2])
+  y <- runif(numPoints, min=ydim[1], max=ydim[2])
+  return(list(pattern = cbind(x, y), winBor=window[[2]],
+              lim=window[[3]], xDiff=window$xDiff, yDiff=window$yDiff))
+}
+
+
+# ANGLES MATRIX AUX FUNCTION
+angleMatrix <- function(points){
+  angles <- matrix(data=NA, nrow=nrow(points), ncol=nrow(points))
+  for(i in 1:nrow(points)){
+    x <- points[i,1]-points[,1]
+    y <- points[i,2]-points[,2]
+    angles[i,] <- ifelse(x!=0, atan(y/x), 0)
+    angles[i,angles[i,]<0] <- angles[i,angles[i,]<0]+pi
+    angles[i,i] <- 0
+  }
+  return(angles)
+}
+
+# ANISOTROPY TESTING PROCEDURE GIVEN ORIGINAL AND REPLICATED VALUES FOR COMPUTATION OF TEST STATISTICS
+testAniso <- function(OGv, Vs, DSS){
+  if(!(DSS%in%c("G","K","T","P"))) stop("DSS must be either 'G', 'K', 'T', or 'P'.")
+  reps <- ncol(Vs)
+  mHat <- apply(Vs, 1, mean, na.rm=TRUE)
+  vector <- OGv-mHat
+  TstatRep <- rep(NA, reps)
+  if(!(DSS=="G")){
+    CHatDiag <- diag(1/apply(Vs, 1, var, na.rm=TRUE))
+    CHatDiag[CHatDiag==Inf] <- 0 
+    Tstat <- t(vector)%*%CHatDiag%*%vector
+    for(i in 1:reps){
+      vecRep <- Vs[,i]-mHat
+      TstatRep[i] <- t(vecRep)%*%CHatDiag%*%vecRep
+    }
+  } else {
+    Tstat <- t(vector)%*%vector
+    for(i in 1:reps){
+      vecRep <- Vs[,i]-mHat
+      TstatRep[i] <- t(vecRep)%*%vecRep
+    }
+  }
+  p.val <- sum(as.numeric(Tstat) < TstatRep, na.rm=TRUE)/sum(!is.na(TstatRep))
+  return(list(p.val=p.val, Tstat=Tstat, TstatRep=TstatRep))
+}
+
+# CYLINDRICAL K-FUNCTION WITH OPTIONAL  CONTRIBUTIONS FOR MPM
+cylK <- function(spatPat, alpha, aspect=NA, r=seq(0,1, length=100),                 
+                 contribution=FALSE){
+  R <- matrix(c(cos(-alpha), -sin(-alpha), sin(-alpha), cos(-alpha)),
+              nrow=2, byrow=TRUE)
+  rotPattern <- t(R%*%t(spatPat[[1]]))
+  
+  lamHat <- nrow(spatPat[[1]])/(spatPat$xDiff*spatPat$yDiff)
+  
+  
+  xDist <- spatPat$xDiff-as.matrix(dist(cbind(spatPat[[1]][,1],0), "euclidean"))
+  yDist <- spatPat$yDiff-as.matrix(dist(cbind(spatPat[[1]][,2],0), "euclidean"))
+  xRotDist <- as.matrix(dist(cbind(rotPattern[,1],0), "euclidean"))
+  yRotDist <- as.matrix(dist(cbind(rotPattern[,2],0), "euclidean"))
+  diag(xRotDist) <- NA
+  diag(xRotDist) <- NA
+  windowSize <- xDist*yDist
+  contributions <- matrix(NA, ncol=length(r), nrow= nrow(spatPat[[1]]))
+  #numerator <- rep(NA, length(r))
+  for(i in 1:length(r)){
+    contributions[,i] <- apply((xRotDist<=r[i] & yRotDist <=(aspect*r[i]))/windowSize, 1, sum, na.rm=TRUE)/lamHat^2
+  }
+  K <- apply(contributions, 2, sum, na.rm=TRUE)
+  if(contribution){
+    return(list(K=K, KContrib = contributions))
+  } else {
+    return(K=K)
+  }
+  
+}
+
+# LOCAL DIRECTIONAL NEAREST NEIGHBOUR DISTANCE DISTRIBUTION
+GLoc<- function(spatPat, alpha, epsilon, r=seq(0,1, length=100), 
+                distance=NA, angles=NA, retMatrices=FALSE, contribution=FALSE) {
+  if(!is.matrix(distance)){
+    distance <- (as.matrix(dist(x=spatPat[[1]], method="euclidean")))
+  }
+  borders <- cbind(spatPat[[1]][,1]-spatPat$winBor[1,1], spatPat$winBor[2,1]-spatPat[[1]][,1],
+                   spatPat[[1]][,2]-spatPat$winBor[1,2], spatPat$winBor[3,2]-spatPat[[1]][,2])
+  distBorder <- apply(borders, 1, min)
+  
+  distQual <- matrix(NA, nrow=nrow(distance), ncol=ncol(distance))
+  if(!is.matrix(angles)){
+    angles <- angleMatrix(spatPat[[1]])
+  }
+  for(i in 1:nrow(spatPat[[1]])){
+    TF <- ((angles[i,] > alpha-epsilon & angles[i,] < alpha+epsilon ) |
+             (angles[i,] > alpha+pi-epsilon & angles[i,] < alpha+pi+epsilon ))
+    distQual[i, TF] <- distance[i, TF]
+  }
+  diag(distQual) <- NA
+  minDist <- apply(distQual, 1, function(vec){ifelse(all(is.na(vec)), NA, min(vec, na.rm=TRUE)) })
+  
+  if(alpha-epsilon>=0 && alpha+epsilon<=pi/2){
+    xMar <- 1*cos(alpha-epsilon)
+    yMar <- 1*cos(pi/2-alpha-epsilon)
+  } else if(alpha-epsilon>=0 && alpha-epsilon<=pi/2  && alpha+epsilon>=pi/2) {
+    xMar <- max(1*cos(alpha-epsilon), 1*cos(pi-alpha-epsilon))
+    yMar <- 1
+  } else if(alpha-epsilon>=pi/2  && alpha+epsilon<=pi){
+    xMar <- 1*cos(pi-alpha-epsilon)
+    yMar <- 2*cos(alpha-epsilon-pi/2)
+  } else {
+    xMar <- 1
+    yMar <- max(abs(1*sin(alpha+epsilon)), 1*sin(pi-alpha-epsilon))
+  }
+  volumes <- (spatPat$xDiff-2*xMar*minDist)*(spatPat$yDiff-2*xMar*minDist)
+  NAs <- which(is.na(volumes)|is.na(minDist))
+  if(length(NAs)>0){
+    volumes2 <- volumes[-NAs]
+    minDist2 <- minDist[-NAs]
+    spatPat2 <- spatPat[[1]][-NAs,]
+  } else{
+    volumes2 <- volumes
+    minDist2 <- minDist
+    spatPat2 <- spatPat[[1]]
+  }
+  contributions <- matrix(NA, ncol=length(r), nrow=nrow(spatPat[[1]]))
+  border <- which(spatPat[[1]][,1]>spatPat[[2]][1,1]+xMar*minDist & spatPat[[1]][,1]<spatPat[[2]][3,1]-xMar*minDist &
+                    spatPat[[1]][,2]>spatPat[[2]][1,2]+yMar*minDist & spatPat[[1]][,2]<spatPat[[2]][3,2]-yMar*minDist)
+  if(length(border)>0){
+    for(i in 1:length(r)){
+      contributions[border,i] <- (minDist[border]<r[i])/volumes[border]
+      contributions[-border,i] <- 0
+      contributions[-NAs,i]
+      
+    }
+    numerator <- apply(contributions, 2, sum, na.rm=TRUE)
+    denominatorContirb <- rep(0, nrow(spatPat[[1]]))
+    denominatorContirb[border] <- (minDist[border]<sqrt(spatPat$xDiff^2+spatPat$yDiff^2))/volumes[border]
+    denominator <- sum(denominatorContirb)
+    if(retMatrices && contribution){
+      return(list(Gloc=numerator/denominator, distance=distance, angles=angles, 
+                  GContrib = list(numContrib = contributions, denContrib = denominatorContirb)))
+    } else if(retMatrices){
+      return(list(Gloc=numerator/denominator, distance=distance, angles=angles))
+    } else if(contribution){
+      return(list(Gloc=numerator/denominator, 
+                  GContrib = list(numContrib = contributions, denContrib = denominatorContirb)))
+    } else {
+      return(numerator/denominator)
+      
+    }
+  } else {
+    if(retMatrices && contribution){
+      return(list(Gloc=rep(0, length(r)), distance=distance, angles=angles, 
+                  GContrib = list(numContrib = matrix(0, ncol=length(r), nrow=nrow(spatPat[[1]])), 
+                                  denContrib = matrix(1, ncol=length(r), nrow=nrow(spatPat[[1]])))))
+    } else if(retMatrices){
+      return(list(Gloc=rep(0, length(r)), distance=distance, angles=angles))
+    } else if(contribution){
+      return(list(Gloc=rep(0, length(r)), 
+                  GContrib = list(numContrib = matrix(0, ncol=length(r), nrow=nrow(spatPat[[1]])), 
+                                  denContrib = matrix(1, ncol=length(r), nrow=nrow(spatPat[[1]])))))
+    } else {
+      return(rep(0, length(r)))
+      
+    }
+  }
+  
+  
+}
+
+# PERIODOGRAM WITH OPTINAL MPM CONTRIBUTIONS
+periodogram <- function(spatPat, p, contribution=FALSE){
+  p1 <- p
+  p2 <- p
+  omega1 <- 2*pi*p1/spatPat$xDiff
+  omega2 <- 2*pi*p2/spatPat$yDiff
+  grid <- as.matrix(expand.grid(omega1, omega2))
+  area <- spatPat$xDiff*spatPat$yDiff
+  if(contribution){
+    contrib <- matrix(NA, ncol=nrow(grid), nrow=nrow(spatPat$pattern))
+    contrib.og <- exp(-1i*grid%*%t(spatPat[[1]]))/sqrt(area)
+    contrib.cc <- Re(contrib.og)-1i*Im(contrib.og)
+    for(i in 1:nrow(grid)){
+      out <- outer(contrib.og[i,], contrib.cc[i,], "*")
+      contrib[,i] <- Re(apply(out,1,sum)+apply(out,2,sum))/2
+    }
+    sdf <- apply(contrib, 2, sum, na.rm=TRUE)
+    
+  } else {
+    DFT <- rowSums(exp(-1i*grid%*%t(spatPat[[1]])))/sqrt(area)
+    sdf <- Re(DFT)^2 + Im(DFT)^2
+  }
+  zero1 <- which(omega1==0)
+  zero2 <- which(omega2==0)
+  matrixSDF <- matrix(NA, nrow=length(omega1), ncol=length(omega2))
+  angles <- matrix(NA, nrow=length(omega1), ncol=length(omega2))
+  ang <- atan((grid[,2]/grid[,1]))
+  for(i in 1:length(omega1)){
+    matrixSDF[i,] <- sdf[((i-1)*length(omega2)+1):((i)*length(omega2))]
+    angles[i,] <- ang[((i-1)*length(omega2)+1):((i)*length(omega2))]
+  }
+  matrixSDF[zero1,zero2] <- NA
+  angles[zero1,zero1] <- NA
+  angles <- angles %% pi
+  
+  
+  if(contribution){
+    return(list(sdf = matrixSDF, omega1=omega1, omega2=omega2, p1, p2, angles=angles,
+                TContrib = contrib))
+  } else {
+    return(list(sdf = matrixSDF, omega1=omega1, omega2=omega2, p1, p2, angles=angles))
+  }
+  if(contribution){
+    return(list(sdf = sdf, omega1=omega1, omega2=omega2, p1, p2, angles=angles,
+                TContrib = contrib))
+  } else {
+    return(list(sdf = sdf, omega1=omega1, omega2=omega2, p1, p2, angles=angles))
+  }
+  
+}
+
+# THETA SPECTRUM
+thetaSpectrum <- function(prdgrm, thetas, epsilon){ # EPSILON IS h in our specification
+  spectrum <- rep(NA, length(thetas))
+  for(i in 1:length(thetas)){
+    consider <- (abs((prdgrm$angles-thetas[i])%%pi) < epsilon)
+    spectrum[i] <- sum(prdgrm$sdf[consider],
+                       na.rm=TRUE)/ sum(rowSums(consider, na.rm=TRUE))
+  }
+  return(spectrum)                 
+}
+
+# STOCHASTIC RECONSTRUCTION ALGORITHM WITH H-FUNCTION
+
+stchRecH <- function(spatPat, rmax=NULL, rlength=NULL, iter=1e4){
+  
+  xrange <- c(spatPat[[2]][1,1], spatPat[[2]][3,1])
+  yrange <- c(spatPat[[2]][2,2], spatPat[[2]][3,2])
+  
+  # SET UP INTEGRATION GRID FOR R
+  if(!is.null(rmax) & !is.null(rlength)){
+    interval <- rmax/rlength
+    rs <- seq(0, rmax, by=interval)
+    
+    originH <- Hest(X=as.ppp(spatPat[[1]], W=owin(xrange=xrange, yrange=yrange)), r = rs, correction = "raw", conditional=FALSE)$raw
+  } else {
+    primH <- Hest(X=as.ppp(spatPat[[1]], W=owin(xrange=xrange, yrange=yrange)), correction = "raw", conditional=FALSE)
+    originH <- primH$raw
+    rs <- primH$r
+    interval <- mean(diff(rs))
+  }
+  N <- nrow(spatPat[[1]])
+  
+  
+  
+  # INITIALISATION - BINOMIAL PROCESS, U, KMatrix
+  curPat <- rHomPois(xdim=xrange, ydim=yrange, numPoints = N)
+  curH <- Hest(X=as.ppp(curPat[[1]], W=owin(xrange=xrange, yrange=yrange)), r = rs, correction = "raw", conditional=FALSE)$raw
+  curD <- sum(interval*(curH-originH)^2)
+  for(i in 1:iter){
+    move <- sample(1:N, 1)
+    propPat <- curPat
+    propPat[[1]][move,] <- c(runif(1, xrange[1], xrange[2]), runif(1, yrange[1], yrange[2]))
+    propH <- Hest(X=as.ppp(propPat[[1]], W=owin(xrange=xrange, yrange=yrange)), r = rs, correction = "raw", conditional=FALSE)$raw
+    propD <- sum(interval*(propH-originH)^2)
+    if(curD >= propD){
+      accept <- 1
+    } else {
+      TS <- (0.1- 0.1*(i/(iter+1))^(1/10))
+      Paccept <- exp((propD-curD)/TS)/100
+      accept <- runif(1, 0, 1)<Paccept
+    }
+    if(accept == 1){
+      curPat <- propPat
+      curH <- propH
+      curD <- propD
+    }
+  }
+  return(curPat)
+}
+
+
+
+
+# PARAMETERS
+theta = pi/6
+xdim = ydim = c(-0.5, 0.5)/2
+
+N <- 1000
+M <- 99
+
+grid_length <- 36
+Gepsilon <- pi/4
+Kaspect <- 0.15
+Tepsilon <- pi*7.5/180
+Tp <- -15:15
+
+iterSR <- 5e3
+
+theta.grid <- seq(0, pi, length=37)[-37]
+r.grid.W <- seq(0, 0.25/2, length=37)[-1]
+
+
+
+registerDoParallel(cores=32)
+
+
+
+loaded <- read.csv("Gibbs1_big_2.csv")[,-1]
+ixs <- which(loaded[,1]=="pattern")
+
+set.seed(7893)
+seeds <- sample(1e6, 1e3)
+
+output <- foreach(l=1:N, .combine=cbind, .export = c("simWind", "angleMatrix","xScalingFactor", "Lest", "Hest",
+                                                     "testAniso","cylK", "GLoc", "periodogram", "thetaSpectrum", "stchRecH",
+                                                     "owin", "xdim", "ydim",
+                                                     "theta", "grid_length", "M", "Gepsilon", "Kaspect", "Tepsilon", "Tp", "theta.grid",
+                                                     "as.ppp", "r.grid.W", "rHomPois", "iterSR"),
+                  .multicombine = TRUE, .maxcombine = N) %dopar% {
+                    set.seed(seeds[l])
+                    window <- simWind(xdim, ydim)
+                    if(l<N){
+                      pat <- list(pattern = loaded[(ixs[l]+1):(ixs[l+1]-1),], winBor=window[[2]],
+                                  lim=window[[3]], xDiff=window$xDiff, yDiff=window$yDiff)    
+                    } else {
+                      pat <- list(pattern = loaded[(ixs[l]+1):nrow(loaded),], winBor=window[[2]],
+                                  lim=window[[3]], xDiff=window$xDiff, yDiff=window$yDiff)  
+                    }
+                    pat[[1]] <- cbind(as.numeric(pat[[1]][,1]), as.numeric(pat[[1]][,2]))
+                    pat[[1]] <- pat[[1]][abs(pat[[1]][,1])<0.25 & abs(pat[[1]][,2])<0.25,]
+
+                    L <- Lest(as.ppp(pat[[1]], W=owin(xdim, ydim)))
+                    max.R <- min(diff(xdim)/4, L$r[1+which(((cumsum(abs(L$theo-L$trans)))/L$r)[-1]==max(((cumsum(abs(L$theo-L$trans)))/L$r)[-1]))])
+                    r.grid.L <- seq(0, max.R, length=grid_length+1)[-1]
+                    secDir <- (theta+pi/2)%%pi
+                    
+                    
+                    Gpref <- GLoc(spatPat=pat, alpha=theta, epsilon=Gepsilon, r=r.grid.W, contribution=FALSE, retMatrices=TRUE)
+                    Gsec <- GLoc(spatPat=pat, alpha=secDir, epsilon=Gepsilon, r=r.grid.W, contribution=FALSE)
+                    Gvec.W <- Gpref$Gloc-Gsec
+                    
+                    Gpref <- GLoc(spatPat=pat, alpha=theta, epsilon=Gepsilon, r=r.grid.L, contribution=FALSE, retMatrices=TRUE)
+                    Gsec <- GLoc(spatPat=pat, alpha=secDir, epsilon=Gepsilon, r=r.grid.L, contribution=FALSE)
+                    Gvec.L <- Gpref$Gloc-Gsec
+                    
+                    Kpref <- cylK(spatPat=pat, alpha=theta, aspect=Kaspect, r=r.grid.W, contribution=FALSE)
+                    Ksec <- cylK(spatPat=pat, alpha=secDir, aspect=Kaspect, r=r.grid.W, contribution=FALSE)
+                    Kvec.W <- Kpref-Ksec
+                    
+                    Kpref <- cylK(spatPat=pat, alpha=theta, aspect=Kaspect, r=r.grid.L, contribution=FALSE)
+                    Ksec <- cylK(spatPat=pat, alpha=secDir, aspect=Kaspect, r=r.grid.L, contribution=FALSE)
+                    Kvec.L <- Kpref-Ksec
+                    
+                    Theta <- periodogram(spatPat=pat, p=Tp, contribution=FALSE)
+                    Tvec <- thetaSpectrum(Theta, theta.grid, Tepsilon)
+                    
+                    vGtile.W <- vGtile.L <- vKtile.W <- vKtile.L <- matrix(NA, nrow= length(r.grid.W), ncol= M)
+                    vTtile <- matrix(NA, nrow= length(theta.grid), ncol= M)
+                    
+                    for(m in 1:M){
+                      recPat <- stchRecH(pat, iter=iterSR)
+                      
+                      prefG <- GLoc(spatPat=recPat, alpha=theta, epsilon=Gepsilon, r=r.grid.W, retMatrices = TRUE)
+                      secG <- GLoc(spatPat=recPat, alpha=secDir, epsilon=Gepsilon, r=r.grid.W,
+                                   distance=prefG$distance, angles=prefG$angles)
+                      vGtile.W[,m] <- prefG$Gloc - secG
+                      prefG <- GLoc(spatPat=recPat, alpha=theta, epsilon=Gepsilon, r=r.grid.L, retMatrices = TRUE)
+                      secG <- GLoc(spatPat=recPat, alpha=secDir, epsilon=Gepsilon, r=r.grid.L,
+                                   distance=prefG$distance, angles=prefG$angles)
+                      vGtile.L[,m] <- prefG$Gloc - secG
+                      #K
+                      prefK <- cylK(spatPat=recPat, alpha=theta, aspect=Kaspect, r=r.grid.W)
+                      secK <- cylK(spatPat=recPat, alpha=secDir, aspect=Kaspect, r=r.grid.W)
+                      vKtile.W[,m] <- prefK-secK
+                      prefK <- cylK(spatPat=recPat, alpha=theta, aspect=Kaspect, r=r.grid.L)
+                      secK <- cylK(spatPat=recPat, alpha=secDir, aspect=Kaspect, r=r.grid.L)
+                      vKtile.L[,m] <- prefK-secK
+                      #Theta spectrum
+                      vTtile[,m] <- thetaSpectrum(prdgrm = periodogram(spatPat=recPat, p=Tp),
+                                                  thetas = theta.grid, epsilon = Tepsilon)
+                    }
+                    test.GW <- testAniso(Gvec.W, vGtile.W, "G")
+                    test.GL <- testAniso(Gvec.L, vGtile.L, "G")
+                    test.KW <- testAniso(Kvec.W, vKtile.W, "K")
+                    test.KL <- testAniso(Kvec.L, vKtile.L, "K")
+                    test.T <- testAniso(Tvec, vTtile, "T")
+                    print(l)
+                    c(test.GW$p.val, test.GW$Tstat, test.GW$TstatRep, test.GL$p.val, test.GL$Tstat, test.GL$TstatRep,
+                      test.KW$p.val, test.KW$Tstat, test.KW$TstatRep, test.KL$p.val, test.KL$Tstat, test.KL$TstatRep,
+                      test.T$p.val, test.T$Tstat, test.T$TstatRep)
+                  }
+
+write.csv(output, "GIBBS_SR_1_SMALL.csv")
