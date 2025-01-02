5fde2ea39c428a01905b71cb978ce99084194bbe
Jakub Pypkowski <99726949+jpypkowski@users.noreply.github.com>
2025-01-02 10:37:08 +0000

    Delete functions directory

--- functions/plotSpatPat.R
+++
@@ -1,9 +0,0 @@
-# PLOTTING A SPATIAL PATTERN ON A RECTANGULAR OBSERVATION WINDOW
-plotSpatPat <- function(spatPat, axes=FALSE, 
-                        xlab="", ylab="", ...){
-  plot(spatPat$pattern, frame.plot = FALSE, axes=axes,
-       xlim=spatPat$lim[,1], ylim=spatPat$lim[,2], 
-       xlab=xlab, ylab=ylab, ...)
-  polygon(x=spatPat$winBor[,1], y=spatPat$winBor[,2], 
-          lty=1, lwd=1, border="darkgrey")
-}
