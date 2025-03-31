0a28614a3175583b6a526d19d1f28bf220af0c61
Jakub Pypkowski <99726949+jpypkowski@users.noreply.github.com>
2025-03-31 10:38:17 +0100

    Updated Thomas inference

--- functions/infer.Thomas.R
+++ functions/infer.Thomas.R
@@ -1,8 +1,21 @@
 # INFERENCE FOR THOMAS PROCESS
-# pattern  - point pattern specified using simWind
-# startpar - initial parameters for the search algorithm (see thomas.estK in spatstat)
 
-# returns a vector containing parentIntensity, sigma, meanClusterSize, respectively
+
+
+# NEW
+
+infer.Thomas <- function(pattern, startpar=c(kappa=10,scale=0.1)){
+  estimates <- rep(NA, 3)
+  xrange <- c(pattern[[2]][1,1], pattern[[2]][3,1])
+  yrange <- c(pattern[[2]][2,2], pattern[[2]][3,2])
+  estimates[1:2] <- thomas.estpcf(as.ppp(pattern$pattern, owin(xrange=xrange, yrange=yrange)), startpar=startpar)$par
+  estimates[2] <- sqrt(estimates[2])
+  estimates[3] <- nrow(pattern$pattern)/estimates[1]/(diff(xrange)*diff(yrange))
+  return(estimates)
+}
+
+
+# OLD
 
 infer.Thomas <- function(pattern, startpar=c(kappa=10,scale=0.1)){
   estimates <- rep(NA, 3)
@@ -13,3 +26,6 @@ infer.Thomas <- function(pattern, startpar=c(kappa=10,scale=0.1)){
   estimates[3] <- nrow(pattern$pattern)/estimates[1]/(diff(xrange)*diff(yrange))
   return(estimates)
 }
+# parent intensity, sigma, avg no of offsprings
+
+# CORRECT
